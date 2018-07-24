d161ad720f60bdcfc70130891aae50ed975bc210
Glitch (charming-knot) <none>
2018-07-24 13:47:33 +0000

    😂📠 Checkpoint ./server.js:1523154/133

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ app.get('/', function(request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
 
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function() {
+  console.log("Hello World");
   console.log('Your app is listening on port ' + listener.address().port);
 });
